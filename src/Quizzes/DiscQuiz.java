2a74dfe66383e8e00b8c3c983ad54d273e186cb6
FSSynthetic <fssynthetic.dev@gmail.com>
2021-01-09 00:23:44 -0500

    Update DiscQuiz.java

--- src/Quizzes/DiscQuiz.java
+++ src/Quizzes/DiscQuiz.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class DiscQuiz {
     
-    public static void JavaMain(String[] args) { // Just a simple briefing message.
+    public static void DiscMain(String[] args) { // Just a simple briefing message.
 
         System.out.println();
         System.out.println("------------------------------------------------------");
@@ -30,7 +30,7 @@ public class DiscQuiz {
 			src.StartMenu.main(null);
 		}
 		else {
-			System.out.println(\n"ERROR:  Invalid Response. Please try again.");
+			System.out.println("\nERROR:  Invalid Response. Please try again.");
 			
 			
 			try {
@@ -49,7 +49,7 @@ public class DiscQuiz {
     public static void StartQuiz(String[] args) {
         int Correct = 0;
 
-        System.out.println(\n"Alright, here we go!");
+        System.out.println("\nAlright, here we go!");
 
         try {
             Thread.sleep(3 * 1000);
@@ -59,15 +59,15 @@ public class DiscQuiz {
         }
 	    // Here begins the strings of questions. Each question in assigned to a variable. The questions variables are later placed into an array.
 
-        String Q1 = \n"------------------------------------------------------\n"
+        String Q1 = "\n------------------------------------------------------\n"
             +   "Question 1:\n"
             +   " \n"
             +   "When was Discord first ever released?\n"
             +   " \n"
-            +   "January 25th, 2015: A\March 6th, 2009: B\nMay 13th, 2015: C\nDecember 2nd, 2012: D\n"
+            +   "January 25th, 2015: A\nMarch 6th, 2009: B\nMay 13th, 2015: C\nDecember 2nd, 2012: D\n"
             +   "------------------------------------------------------";
 
-        String Q2 = \n"------------------------------------------------------\n"
+        String Q2 = "\n------------------------------------------------------\n"
             +   "Question 2:\n"
             +   " \n"
             +   "Who is the founder of Discord?\n"
@@ -75,7 +75,7 @@ public class DiscQuiz {
             +   "Daniel Ek: A\nLarry Page: B\nBobby Murphy: C\nJason Citron: D\n"
             +   "------------------------------------------------------";
 
-        String Q3 = \n"------------------------------------------------------\n"
+        String Q3 = "\n------------------------------------------------------\n"
             +   "Question 3:\n"
             +   " \n"
             +   "What is the name of Discord's mascot?\n"
@@ -83,7 +83,7 @@ public class DiscQuiz {
             +   "Wumpus: A\nDragon: B\nClyde: C\nGhostface Chillah: D\n"
             +   "------------------------------------------------------";
 
-        String Q4 = \n"------------------------------------------------------\n"
+        String Q4 = "\n------------------------------------------------------\n"
             +   "Question 4:\n"
             +   " \n"
             +   "What name did Discord officially give to their logo?\n"
@@ -91,7 +91,7 @@ public class DiscQuiz {
             +   "Wumpus: A\nHypno: B\nLarry: C\nClyde: D\n"
             +   "------------------------------------------------------";
 
-        String Q5 = \n"------------------------------------------------------\n"
+        String Q5 = "\n------------------------------------------------------\n"
             +   "Question 5:\n"
             +   " \n"
             +   "What id Discord's subscription service called?\n"
@@ -99,7 +99,7 @@ public class DiscQuiz {
             +   "Hypesquad: A\nNitro: B\nTOS: C\nPartner: D\n"
             +   "------------------------------------------------------";
 
-        String Q6 = \n"------------------------------------------------------\n"
+        String Q6 = "\n------------------------------------------------------\n"
             +   "Question 6:\n"
             +   " \n"
             +   "What badge is unlocked when helping Discord find and fix bugs?\n"
@@ -107,7 +107,7 @@ public class DiscQuiz {
             +   "Bug Hunter Badge: A\nHelper Badge: B\nFixer Badge: C\nPartner Badge: D\n"
             +   "------------------------------------------------------";
 
-        String Q7 = \n"------------------------------------------------------\n"
+        String Q7 = "\n------------------------------------------------------\n"
             +   "Question 7:\n"
             +   " \n"
             +   "When was Discord Dark Mode first released?"
@@ -115,15 +115,15 @@ public class DiscQuiz {
             +   "January 19th, 2016: A\nAugust 6th, 2015: B\nSeptember 3rd, 2015: C\nJune 5th, 2016: D\n"
             +   "------------------------------------------------------";
 
-        String Q8 = \n"------------------------------------------------------\n"
+        String Q8 = "\n------------------------------------------------------\n"
             +   "Question 8:\n"
             +   " \n"
             +   "What is the name of the robot hamster that pops up on Discord's 404 page?\n"
             +   " \n"
-            +   "ChatterBox: A\Marky: B\nWumperston: C\Nelly: D\n"
+            +   "ChatterBox: A\nMarky: B\nWumperston: C\nNelly: D\n"
             +   "------------------------------------------------------";
 
-        String Q9 = \n"------------------------------------------------------\n"
+        String Q9 = "\n------------------------------------------------------\n"
             +   "Question 9:\n"
             +   " \n"
             +   "What was Discord Inc. originally called?\n"
@@ -131,7 +131,7 @@ public class DiscQuiz {
             +   "Game Talk: A\nDisc Space: B\nOpenFeint: C\nHammer and Chisel Inc.: D\n"
             +   "------------------------------------------------------";
 
-        String Q10 = \n"------------------------------------------------------\n"
+        String Q10 = "\n------------------------------------------------------\n"
             +   "Question 10:\n"
             +   " \n"
             +   "What discord badge represents the old Hammer and Chisel Inc. logo?\n"
@@ -159,10 +159,9 @@ public class DiscQuiz {
             }
             else{
 		System.out.println("Incorrect");
-		System.out.println("The correct answer was" Character.toLowerCase(Answers[i])));
+		System.out.println("The correct answer was" + Character.toLowerCase(Answers[i]));
             }
         }
-
 
         // End of Quiz Questions.
 
